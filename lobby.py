52b341978a99ce2cfab5e2451667dcd981d5231c
rinlover1014 <kkmmjj5715@gmail.com>
2026-09-03 16:26:18 +0900

    Create lobby base

---
+++ lobby.py
@@ -0,0 +1,15 @@
+import pygame as pg
+
+
+class Lobby:
+    def __init__(self, screen):
+        self.screen = screen
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill((20, 20, 40))
+
+    def handle_event(self, event):
+        pass
