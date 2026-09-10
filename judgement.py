d3b43edeba5f0cd498db8911ab6f92e0e4b44184
rinlover1014 <kkmmjj5715@gmail.com>
2026-09-10 16:14:50 +0900

    Add Judgement and JudgementSystem classes
    Implemented a Judgement system to evaluate note timing accuracy.
    test code

--- judgement.py
+++ judgement.py
@@ -1,1 +1,40 @@
+from enum import Enum
 
+
+class Judgement(Enum):
+    PERFECT = "PERFECT"
+    GREAT = "GREAT"
+    GOOD = "GOOD"
+    BAD = "BAD"
+    MISS = "MISS"
+
+
+class JudgementSystem:
+    def judge(self, note_time, input_time):
+        difference = abs(note_time - input_time)
+
+        if difference <= 0.05:
+            return Judgement.PERFECT
+
+        elif difference <= 0.10:
+            return Judgement.GREAT
+
+        elif difference <= 0.15:
+            return Judgement.GOOD
+
+        elif difference <= 0.20:
+            return Judgement.BAD
+
+        else:
+            return Judgement.MISS
+
+if __name__ == "__main__":
+    judgement_system = JudgementSystem()
+
+    note_time = 10.0
+
+    print(judgement_system.judge(note_time, 10.01).value)
+    print(judgement_system.judge(note_time, 10.08).value)
+    print(judgement_system.judge(note_time, 10.13).value)
+    print(judgement_system.judge(note_time, 10.18).value)
+    print(judgement_system.judge(note_time, 10.50).value)
